3a72fd49b47209adc822474c8cd3a61034816989
Priyanka Pudi <priyankapudi4u@gmail.com>
2025-07-02 13:00:57 +0530

    Content setup for client

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-red-500 text-3xl" >Hello world!</h1>
+    </>
+  )
+}
+
+export default App
