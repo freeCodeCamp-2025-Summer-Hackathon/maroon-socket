e3dfec8094c4fda845dd8a5001aaa52d3e41cf50
Priyanka Pudi <priyankapudi4u@gmail.com>
2025-07-03 14:07:24 +0530

    Sign up (#17)
    * Finished SignUp Screen
    
    * Created Sign-up and login forms

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,49 @@
+
+import { useState } from "react";
+import { FaUserPen } from "react-icons/fa6";
+import { MdLockOutline } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+function Login(){
+    const [verification,setVerification] = useState({email:"", password:""});
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(verification);
+    }
+    return (
+        
+         <div className="bg-[#FEF7E7] rounded-2xl shadow-2xl overflow-hidden w-full flex">
+            <div className="flex flex-col items-start gap-4 py-32 px-20 w-full">
+                <h1 className="text-3xl font-semibold text-[#2B4540]" >Sign In</h1>
+                <form className="w-full flex flex-col justify-center items-start gap-4">
+                        <div className="w-full flex justify-center items-center gap-4 rounded-lg border-[1px] border-gray-400 py-3 px-5 shadow">
+                        <span><FaUserPen className="text-2xl text-gray-600"/></span>
+                        <input className="w-full border-none bg-transparent focus:outline-none text-gray-500" type="text" value= {verification.email} onChange={(e)=>setVerification({...verification, email: e.target.value})} placeholder="Your mail id" />
+                        </div>
+                        <div className="w-full flex justify-center items-center gap-4 rounded-lg border-[1px] border-gray-400 py-3 px-5 shadow">
+                        <span><MdLockOutline className="text-2xl text-gray-600"/></span>
+                        <input className="w-full border-none bg-transparent focus:outline-none text-gray-500" type="text" value = {verification.password} onChange={(e) => setVerification({...verification, password: e.target.value})}placeholder="Your Password" />
+                        </div>
+                        <div className="flex justify-center items-center gap-3 mt-6">
+                            <input type="checkbox" className="bg-transparent size-4"></input>
+                            <div className="text-base">Remember me</div>
+                        </div>
+                        <button className="w-40 h-12 p-3 bg-[#29433F] text-white cursor-pointer rounded-lg" onClick={(e)=>handleSubmit(e)}>Login</button>
+                        
+                </form>
+                        <div className="flex gap-1 py-4 mt-3">
+                            Don't have an account ?
+                            <div className="text-[#29433F] cursor-pointer hover:underline"><Link to="/"
+                            >Register here</Link>
+                            </div>
+                        </div>
+                       
+            </div>
+        </div>
+        
+        
+    )
+}
+
+export default Login;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
+import Login from './components/Login'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "login",
+    element: <Login />
+  },
+  
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
