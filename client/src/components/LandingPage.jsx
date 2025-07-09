06a5b770de654a907510b0eb60c220c93cd17c6c
Souptik-Mu <111554072+Souptik-Mu@users.noreply.github.com>
2025-07-09 22:25:22 +0530

    fix: form fields types (#46)

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -34,7 +34,7 @@ function LandingPage() {
                     <img src={heroImage} alt="hero image" />
                 </div>
             </section>
-            <div class="w-40 h-1 bg-[#2d6a4f] rounded-full mx-auto my-4"></div>
+            <div className="w-40 h-1 bg-[#2d6a4f] rounded-full mx-auto my-4"></div>
             {/*Testimonials*/}
             <section className="py-10 px-6">
                 <h2 className="text-3xl font-bold text-center mb-8" >What Our Users Are Saying</h2>
@@ -50,7 +50,7 @@ function LandingPage() {
                     </div>
                 </div>
             </section>
-            <div class="w-40 h-1 bg-[#2d6a4f] rounded-full mx-auto my-4"></div>
+            <div className="w-40 h-1 bg-[#2d6a4f] rounded-full mx-auto my-4"></div>
             {/*Explore*/}
             <section className="py-10 px-6 ">
                 <h2 className="text-3xl font-bold text-center mb-8" >Explore the app</h2>
