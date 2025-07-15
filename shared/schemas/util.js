81b2075ab3533e6d218fbd336129ab3c4e5483d4
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-07-15 22:19:54 +0530

    refactor: add jsDoc to explain flatten error

--- shared/schemas/util.js
+++ shared/schemas/util.js
@@ -1,10 +1,18 @@
 import { z } from 'zod';
 
+/**
+ * This function will take a zodError and convert it into an easier to use format.
+ * example output-
+ * ``{"username": "username cannot be empty",
+ *  "fullname": "fullname must have at least 5 characters"}``
+ * @param {*} zodError
+ * @returns {object}
+ */
+
 export function flattenError(zodError) {
     const treefiedError = z.treeifyError(zodError);
 
     if (treefiedError.properties !== undefined) {
-        //assert properties type otherwise typescript will complain
         const properties = treefiedError.properties;
         const flattenedError = {};
 
