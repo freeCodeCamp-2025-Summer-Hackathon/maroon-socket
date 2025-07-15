ce6a97ed341037325f6335d63aaf882bb68a6284
Souptik Mullick <souptikmullick0692@gmail.com>
2025-07-15 15:50:43 +0530

    fix: tweaked the design a bit.

--- client/src/pages/DetailedPost.jsx
+++ client/src/pages/DetailedPost.jsx
@@ -31,22 +31,32 @@ const DetailedPost = () => {
         console.log('New Comment:', newComment);
     };
     return (
-        <div className="w-screen h-screen flex flex-col bg-white gap-8 m-8">
+        <div className="w-screen h-screen flex flex-col bg-white max-w-fit gap-6 m-8">
             {/*Post details*/}
-            <div className="flex flex-col gap-2 mb-2 rounded-md shadow px-4 m-4">
-                <h5 className="font-semibold text-base">{post.UserName}</h5>
+            <div className="flex flex-col gap-1 mb-2 rounded-md border-2 shadow px-4 m-4">
+                <div className="flex flex-row gap-2 py-2">
+                    <div className="h-8 w-8 rounded-full text-white font-bold bg-[#3a6b3d] flex items-center justify-center">
+                        {post.UserName.charAt(0).toUpperCase()}
+                    </div>
+                    <h5 className="font-semibold text-base flex items-center">
+                        {post.UserName}
+                    </h5>
+                </div>
                 <h2 className="text-2xl font-bold mb-2">{post.title}</h2>
                 <p className="mb-4 text-gray-700">{post.content}</p>
             </div>
+            
             {/*Comments*/}
-
-            <ul className="space-y-4 mb-6 flex flex-col items-justify-start px-4 mx-4">
+            <ul className="gap-3 flex flex-col items-justify-start px-4 ml-4">
                 {comments.map((comment, i) => (
-                    <li key={i} className="flex flex-col items-start gap-1">
+                    <li
+                        key={i}
+                        className="flex flex-col items-start gap-1 p-2 border rounded-md shadow-md"
+                    >
                         <div className="font-semibold text-black">
                             {comment.UserName}
                         </div>
-                        <p className="mb-4 ml-6 text-gray-700">
+                        <p className="mb-3 ml-6 text-gray-700">
                             {comment.content}
                         </p>
                     </li>
@@ -56,10 +66,10 @@ const DetailedPost = () => {
             {/*Add comment feild*/}
             <form
                 onSubmit={handleSubmit}
-                className="absolute bottom-2 left-0 w-full flex items-center gap-2 px-6 py-4"
+                className="absolute bottom-2 left-0 w-full flex items-center gap-2 px-6 py-4 ml-6"
             >
                 <input
-                    className="border border-gray-300 rounded-full p-2 w-full max-w-md mb-4"
+                    className="border border-gray-300 rounded-full p-2 w-full max-w-md"
                     type="text"
                     value={newComment}
                     placeholder="Add a comment..."
@@ -67,9 +77,9 @@ const DetailedPost = () => {
                 />
                 <button
                     type="submit"
-                    className="p-1 rounded-full bg-[#3a6b3d] text-white hover:bg-[#29423e]"
+                    className="p-2 rounded-full bg-[#3a6b3d] text-white hover:bg-[#29423e]"
                 >
-                    <CiPaperplane size={30} />
+                    <CiPaperplane size={24} />
                 </button>
             </form>
         </div>
