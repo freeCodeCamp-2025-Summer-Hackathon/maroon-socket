fa885f9fbf49356333e5f22cad6eefb919d23302
Ayushi Baliyan <147339218+Compilearner@users.noreply.github.com>
2025-07-19 21:06:52 +0530

    Fix/post card component (#96)
    * fix: truncate post content based on length
    
    * Format and lint checks

--- client/src/components/PostBtn.jsx
+++ client/src/components/PostBtn.jsx
@@ -9,7 +9,7 @@ export default function PostBtn() {
         <>
             <button
                 onClick={() => setShowModal(true)}
-                className="bg-[#2C6A4E] flex justify-center gap-2 w-60 px-6 py-2 rounded-md text-white font-semibold text-lg font-poppins"
+                className="bg-btn flex justify-center gap-2 w-fit md:w-60 px-6 py-2 rounded-md text-white font-semibold text-lg font-poppins"
             >
                 <MdCreate className="self-center" />
                 Create a Post
--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,8 +1,12 @@
 import { FaUser } from 'react-icons/fa6';
+import { Link } from 'react-router-dom';
 
-const PostCard = ({ post }) => {
+const PostCard = ({ post, fullContent = false }) => {
+    const maxLength = 300;
+    const isLong = post?.description.length > maxLength;
+
     return (
-        <div className="w-full h-fit rounded-md shadow shadow-gray-400 px-8 py-14 bg-secondary relative ">
+        <div className="w-full h-fit rounded-md shadow shadow-gray-400 px-4 md:px-8 py-14 bg-white relative ">
             {/** USERNAME CONTAINER */}
             <div className="flex justify-start items-center gap-3 px-8 py-4">
                 <span aria-label="User-icon">
@@ -17,14 +21,29 @@ const PostCard = ({ post }) => {
                     {post.title}
                 </h2>
                 <div className="w-full h-fit bg-gray-200 p-4 rounded-md">
-                    <p className="text-base">{post.description}</p>
+                    <p className="text-base">
+                        {fullContent
+                            ? post?.description
+                            : post?.description.slice(0, maxLength)}
+                        {!fullContent && isLong && (
+                            // Change the path accordingly
+                            <Link to={`/`}>
+                                <span className="font-bold text-black">
+                                    ......
+                                </span>
+                                <button className="text-black hover:text-blue-700 underline text-base font-bold cursor-pointer">
+                                    Read More
+                                </button>
+                            </Link>
+                        )}
+                    </p>
                 </div>
             </div>
 
             {/** TAG CONTAINER */}
             <span
                 aria-label={`Post category: ${post.tag}`}
-                className={`absolute top-14 right-16 px-2 w-24 h-7 flex justify-center items-center rounded-md shadow shadow-gray-400 ${post.tag === 'Tip' ? 'bg-blue-600' : 'bg-green-600'}`}
+                className={`absolute top-20 right-12 md:top-14 md:right-16 px-2 w-24 h-7 flex justify-center items-center rounded-md shadow shadow-gray-400 ${post.tag === 'Tip' ? 'bg-blue-600' : 'bg-green-600'}`}
             >
                 <p className="text-white font-bold">
                     {post.tag === 'Tip' ? 'Tip' : 'Question'}
