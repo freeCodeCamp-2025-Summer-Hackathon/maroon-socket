c3272d8c8b6bd5ca86dc03cfc30207bb0ea1648b
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-07-20 13:01:53 +0530

    feat:display fetched posts on community

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,36 +1,27 @@
+import { useEffect, useState } from 'react';
 import PostBtn from '../components/PostBtn';
 import PostCard from '../components/PostCard';
 
-// Dummy testing
-const posts = [
-    {
-        id: 1,
-        userName: 'EmilyGardens',
-        title: 'Why are my monstera leaves turning yellow?',
-        tag: 'Question',
-        description:
-            "I’ve been watering my monstera once a week and it's placed near a bright window. But recently, the leaves have started turning yellow. Could this be due to overwatering or something else?"
-    },
-    {
-        id: 2,
-        userName: 'PlantParentTom',
-        title: 'The Complete Guide to Nurturing Healthy Indoor Plants',
-        tag: 'Tip',
-        description: `Taking care of indoor plants isn't just about watering them occasionally
-             1. Know Your Plant's Needs
-             2. Don’t Overwater
-             3. Use Pots with Drainage
-             4. Give Them the Right Light
-             5. Rotate Regularly
-             6. Dust the Leaves
-             7. Repot When Necessary
-             8. Fertilize — But Not Too Much
-             9. Watch for Pests
-             10. Be Patient and Observant, Caring for plants is more about attention than perfection`
-    }
-];
-
 const Community = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const apiUrl = import.meta.env.VITE_API_URL;
+
+        const fetchPosts = async () => {
+            try {
+                const res = await fetch(`${apiUrl}/api/post`);
+                const json = await res.json();
+                setPosts(json.data);
+            } catch (error) {
+                console.log(error);
+                alert('someting went wrong');
+            }
+        };
+
+        fetchPosts();
+    }, []);
+
     return (
         <div className="w-full h-full md:px-12 py-16 flex flex-col justify-center items-center gap-12 relative pt-16 mt-20 bg-secondary">
             {/** Welcome message */}
