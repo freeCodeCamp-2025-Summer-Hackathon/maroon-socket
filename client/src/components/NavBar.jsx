49218877e8d40c309b7b7df95742f1e57700b71e
jait <jai8.jacob@gmail.com>
2025-07-22 21:22:45 +0530

    fix: make navbar logo responsive (#113)
    * fix: make navbar logo responsive

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,19 +3,24 @@ import Plantlogo from '../assets/logos/green_logo.svg';
 
 function NavBar() {
     return (
-        <nav className="bg-white shadow-md flex items-center p-6 fixed top-0 left-0 w-full z-50">
-            <div className="w-full flex justify-between items-center px-8">
-                <div className="w-28 flex justify-center items-center">
-                    <img src={Plantlogo} className="object-cover w-full h-7" />
+        <nav className="bg-white shadow-md flex items-center fixed top-0 left-0 w-full z-50">
+            <div className="w-full flex justify-between items-center px-4 sm:px-6 lg:px-8">
+                <div className="flex items-center">
+                    <img
+                        src={Plantlogo}
+                        alt="Plant Logo"
+                        className="w-24 lg:w-28 py-4"
+                    />
                 </div>
-                <ul className=" flex gap-5 px-20 text-gray-600">
-                    <li className="hover:underline underline-offset-8 font-semibold font-poppins text-primary ">
+
+                <ul className="flex gap-3 md:gap-5 lg:px-20 text-gray-600">
+                    <li className="hover:underline underline-offset-8 font-semibold font-poppins text-primary">
                         <Link to={'/'}>Home</Link>
                     </li>
-                    <li className=" hover:underline underline-offset-8 font-semibold font-poppins ">
+                    <li className="hover:underline underline-offset-8 font-semibold font-poppins">
                         <Link to="/community">Community</Link>
                     </li>
-                    <li className="hover:underline underline-offset-8 font-semibold font-poppins  ">
+                    <li className="hover:underline underline-offset-8 font-semibold font-poppins">
                         <Link to="/login">Login</Link>
                     </li>
                 </ul>
