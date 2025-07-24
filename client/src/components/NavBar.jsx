07e94b603b8fc09d7e30c1f3a013a1a39a6aa7b6
jx <jai8.jacob@gmail.com>
2025-07-24 15:03:49 +0530

    refactor: myPlant is also added to conditional render

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -26,17 +26,22 @@ function NavBar() {
                     <li className="hover:underline underline-offset-8 font-semibold font-poppins text-primary">
                         <Link to={'/'}>Home</Link>
                     </li>
-                    <li className="hover:underline underline-offset-8 font-semibold font-poppins">
-                        <Link to={'/userHome'}>My Plants</Link>
-                    </li>
+
+                    {isLoggedIn ? (
+                        <>
+                            {' '}
+                            <li className="hover:underline underline-offset-8 font-semibold font-poppins">
+                                <Link to={'/userHome'}>My Plants</Link>
+                            </li>
+                            <li className="hover:underline underline-offset-8 font-semibold font-poppins">
+                                <Link to="/profile">Profile</Link>
+                            </li>{' '}
+                        </>
+                    ) : null}
+
                     <li className="hover:underline underline-offset-8 font-semibold font-poppins">
                         <Link to="/community">Community</Link>
                     </li>
-                    {isLoggedIn ? (
-                        <li className="hover:underline underline-offset-8 font-semibold font-poppins">
-                            <Link to="/profile">Profile</Link>
-                        </li>
-                    ) : null}
 
                     {isLoggedIn ? null : (
                         <li className="hover:underline underline-offset-8 font-semibold font-poppins">
